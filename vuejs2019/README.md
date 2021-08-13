44f55afd88b8f275e56544e0ed0eb2da20ce88d9
Darlley <darlleybrito@gmail.com>
2021-08-12 21:46:07 -0400

    aula 1 e 2

---
+++ vuejs2019/README.md
@@ -0,0 +1,7 @@
+# CURSO VUEJS2019 - HIGH TECH CURSOS
+
+### *Playlist Completa Abaixo*
+
+> [![Primeiro Vídeo](https://i.ytimg.com/vi/fpG_COa531M/hqdefault.jpg?sqp=-oaymwEXCNACELwBSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLADzaFKKh5xG0ak0XQsMHvRYQssFQ)](https://www.youtube.com/playlist?list=PLKvsMn7xWutbFX1NpzjEdn44OOyDN5ax6) 
+>
+> ![Logotipo](https://vuejs.org/images/logo.svg)
---
+++ vuejs2019/todo-list/js/main.js
@@ -0,0 +1,21 @@
+new Vue({
+    el: '#app',
+    data: {
+        title: 'Todo-List',
+        namebtn: "Adicionar"
+    }
+})
+
+/* JAVASCRIPT VANILA - TODO LIST
+
+let tarefas = []
+// let container = document.querySelector('div#container');
+let btn = document.querySelector('button#btn-valores');
+btn.addEventListener('click', function(){
+
+    let inputAdd = document.querySelector('input#valores').value;
+    // container.innerHTML += `<p class="itens">${inputAdd}</p>`;
+    tarefas.push(inputAdd)
+    document.querySelector('#lista').innerHTML += `<li class="itens">${inputAdd}</li>`
+
+}); */
